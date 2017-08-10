5c6a11962e9c1b03b483c4a15577c014b6450dfa
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-10 03:02:54 -0300

    Dont need the command key on mac

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // Console.ReadKey(); dont need this on a mac
         }
     }
 }
